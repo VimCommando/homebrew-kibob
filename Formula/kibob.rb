c53cf9b231664162d1892ebc8f54c5377931bacb
Ryan Eno <ryan.eno@elastic.co>
2026-02-27 12:13:54 -0800

    kibob 0.2.0

--- Formula/kibob.rb
+++ Formula/kibob.rb
@@ -1,8 +1,8 @@
 class Kibob < Formula
   desc "Git-inspired CLI tool for managing Kibana saved objects"
   homepage "https://github.com/VimCommando/kibana-object-manager"
-  url "https://github.com/VimCommando/kibana-object-manager/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "caa260768f03f66d4c50b0e64ff12e22e68e36b8b422d8f947c39b143ff8dce6"
+  url "https://github.com/VimCommando/kibana-object-manager/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "2ade53ff7193b9bb9008a1a0bcfa6d8d1a7c16c4ff64d7572ac436fb7f57ee37"
   license "Apache-2.0"
 
   depends_on "rust" => :build
